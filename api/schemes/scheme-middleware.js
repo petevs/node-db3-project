fc020596018dae7469d569d83b96fcced4cd7d59
Pete VS <pete_vs88@me.com>
2021-03-02 20:57:29 -0500

    passing all tests

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -37,6 +37,11 @@ const checkSchemeId = async (req, res, next) => {
   }
 */
 const validateScheme = (req, res, next) => {
+  if(!req.body.scheme_name || typeof req.body.scheme_name !== 'string'){
+    res.status(400).json({
+      message: "invalid scheme_name"
+    })
+  }
   next()
 }
 
@@ -50,6 +55,12 @@ const validateScheme = (req, res, next) => {
   }
 */
 const validateStep = (req, res, next) => {
+  if(!req.body.instructions || typeof req.body.instructions !== 'string' || typeof req.body.step_number !== 'number' || req.body.step_number < 1){
+    res.status(400).json({
+      "message": "invalid step"
+    })
+  }
+
   next()
 }
 
